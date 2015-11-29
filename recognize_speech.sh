91db67742f42003ddae93cf4dcfaa96cd51ec997
RaphaelVogel <raphael.vogel@icloud.com>
2015-11-29 18:20:39 +0100

    add recognition to server

--- recognize_speech.sh
+++ recognize_speech.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-KEY="xxxxxxxxxxxxxxxxx"
+KEY="AIzaSyDkxrlx2acbRLILXVU9_UgnRqUXLcfRoWs"
 URL="https://www.google.com/speech-api/v2/recognize?output=json&lang=de-de&key=$KEY"
 
 arecord -D plughw:0,0 -f cd -t wav -d 4 -q -r 16000 | flac - -s -f --best --sample-rate 16000 -o file.flac;
 
 wget -q -U "Mozilla/5.0" --post-file file.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "$URL" >stt.txt
 
-cat stt.txt
-
 rm file.flac  > /dev/null 2>&1
-#
