df73bbe7532a5031f7ff2934779e0d2449d834b2
RaphaelVogel <raphael.vogel@icloud.com>
2015-11-30 20:32:53 +0100

    fix return value

--- web/routes.py
+++ web/routes.py
@@ -1,6 +1,9 @@
 from bottle import route, static_file, request, HTTPResponse
 import json
 import subprocess
+import logging
+
+logger = logging.getLogger("audiospeech_logger")
 
 
 @route('/')
@@ -19,8 +22,10 @@ def speech_recognizer():
     subprocess.call('. ./audiospeech/recognize_speech.sh', shell=True)
     with open("./audiospeech/stt.txt") as f:
         out = f.read()
-    response = json.loads(out).split('\n', 1)
+
+    response = out.split('\n', 1)
     if len(response) <= 1:
-        return dict(text="Could not recognize")
+        return dict(text="Could not recognize anything")
+
     text = json.loads(response[1])['result'][0]['alternative'][0]['transcript']
-    return dict(text="text")
+    return dict(recognized_text=text)
