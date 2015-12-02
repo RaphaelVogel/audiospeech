d9c7727800d9b3278193d51c1fe4fd1124b6b94f
RaphaelVogel <raphael.vogel@icloud.com>
2015-12-02 07:03:56 +0100

    change rec

--- recognize_speech.sh
+++ recognize_speech.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KEY="AIzaSyDkxrlx2acbRLILXVU9_UgnRqUXLcfRoWs"
+KEY="xxxxxxxxxxx"
 URL="https://www.google.com/speech-api/v2/recognize?output=json&lang=de-de&key=$KEY"
 
 arecord -D plughw:0,0 -f cd -t wav -d 3 -q -r 16000 | flac - -s -f --best --sample-rate 16000 -o ./audiospeech/file.flac;
