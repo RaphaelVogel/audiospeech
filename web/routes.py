aef3870c705a0235a008f442dc6492f4195c1905
RaphaelVogel <raphael.vogel@icloud.com>
2016-02-07 17:06:23 +0100

    insert some logging

--- web/routes.py
+++ web/routes.py
@@ -61,6 +61,7 @@ def decrease_volume():
 
 @route('/recognize')
 def speech_recognizer():
+    logger.info("In speech_recognizer method")
     stop_radio()
     intent = None
     try:
@@ -74,8 +75,7 @@ def speech_recognizer():
         evaluate_intent(intent['outcomes'][0]['intent'], intent['outcomes'][0]['confidence'],
                         intent['outcomes'][0]['entities'])
     except Exception as e:
-        logger.error("Response: " + str(intent))
-        logger.error("Got exception: " + str(e))
+        logger.error("Got exception: %s", str(e))
         say("Entschuldigung, ich habe das nicht verstanden")
         return dict(recognized_text="Could not recognize anything")
 
@@ -83,15 +83,17 @@ def speech_recognizer():
 
 
 def say(text):
-    subprocess.call('pico2wave --lang=de-DE --wave=/tmp/test.wav "' + text + '"; aplay /tmp/test.wav;rm /tmp/test.wav', shell=True)
+    subprocess.call('pico2wave --lang=de-DE --wave=/tmp/test.wav "' + text + '" && aplay /tmp/test.wav && rm /tmp/test.wav', shell=True)
 
 
 # ---- EVALUATION ------------------------------------------------------------------------------------------
 def evaluate_intent(intent, confidence, entities):
+    logger.info("In evaluate_intent: %s  %s  %s", str(intent), str(confidence), str(entities))
     if entities:
         entity_name = entities.keys()[0]
         value = entities[entity_name][0]['value']
     if float(confidence) < 0.6 or intent == "UNKNOWN":
+        logger.info("Confidence %s smaller than 0.6", confidence)
         say("Diesen Befehl kenne ich nicht")
         return
     if intent == 'erste_bundesliga':
@@ -105,11 +107,6 @@ def evaluate_intent(intent, confidence, entities):
         say("Ich bin Prinzessin Lea")
     elif intent == "bild_des_tages":
         requests.get('http://192.168.1.18:8080/picOfTheDay')
-    elif intent == 'reminder':
-        requests.get('http://192.168.1.18:8080/currentTime')
-        if value == "1":
-            say("Ok, Alarm in einer Minute")
-        else:
-            say("Ok, Alarm in " + value + " Minuten")
     else:
+        logger.info("Intent %s is nor known", intent)
         say("Diesen Befehl kenne ich nicht")
